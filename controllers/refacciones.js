3c7ddf588949040e6ff7914ac8385868a39047d5
Alfredo Rouse <alfredo-7@live.com>
2016-05-03 22:33:27 -0700

    Se modificó la consulta a mongo, ahora solo retorna el array de las refacciones que concuerden y no todo el documento

--- controllers/refacciones.js
+++ controllers/refacciones.js
@@ -3,7 +3,26 @@ var Refaccion = mongoose.model('refacciones');
 
 exports.find = function (req, res) {
     console.log(req.query);
-    Refaccion.find(req.query ,function (err, refactions) {
+    // console.log(req.query['refaccion.nombre']);
+    // Refaccion.find(req.query, 'refaccion', function (err, refactions) {
+    //     if (err) res.send(500, err.message);
+
+    //     res.json(refactions);
+    // });
+    var projection = {
+        $project: {
+            refaccion: {
+                $filter: {
+                    input: '$refaccion', 
+                    as: 'ref', 
+                    cond: {
+                        $eq: ['$$ref.nombre', req.query['refaccion.nombre']]
+                    }
+                }
+            }
+        }
+    };
+    Refaccion.aggregate([{ $match: req.query }, projection], function (err, refactions) {
         if (err) res.send(500, err.message);
 
         res.json(refactions);
